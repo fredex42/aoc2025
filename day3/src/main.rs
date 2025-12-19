b07c30e67afdd01d3e8a2fe0f665b119389bde0b
Andy Gallagher <fredex42@gmail.com>
2025-12-19 20:51:58 +0000

    working day 3 part 2 solution

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -2,7 +2,6 @@ use std::error::Error;
 use std::fs::File;
 use std::io::Read;
 use std::num::ParseIntError;
-use std::fmt::format;
 
 #[derive(Debug)]
 pub struct BatteryBank {
@@ -56,6 +55,41 @@ impl BatteryBank {
 
         (highest_val * 10) + second_highest
     }
+
+    /**
+     * Calculate the "joltage" (defined in the problem) for the battery bank.
+     * This is now the highest 12-numbered value that can be gained by taking one
+     * digit as the 10^x and another to the right of it as the 10^(x-1) etc.
+     */
+    pub fn max_joltage_v2(&self) -> u64 {
+        fn find_next_highest(content: &Vec<u32>, pow:usize,start_pos:usize) -> (u64, usize) {
+            //If we are e.g. at power 10, there must be at least 10 other values following us in order to be valid.
+            //Practically this means we cut off at len - 10
+            if content.len()==0 {
+                return (0, 0)
+            }
+            let mut highest:u64 = 0;
+            let mut highest_index:usize = 0;
+            for i in start_pos..(content.len() - pow) {
+                if u64::from(content[i]) > highest {
+                    highest = content[i].into();
+                    highest_index = i;
+                }
+            }
+
+            (highest * 10_u64.pow(pow.try_into().unwrap()), highest_index+1)    //next start position is current highest index + 1
+        }
+
+        let mut sum:u64 = 0;
+        let mut pos:usize = 0;
+        for pow in (0..12).rev() {
+            let (next_value, next_pos) = find_next_highest(&self.content, pow, pos);
+            sum += next_value;
+            pos = next_pos;
+        }
+
+        sum
+    }
 }
 
 pub fn parse_input(content:&str) -> Result<Vec<BatteryBank>, Box<dyn Error>> {
@@ -76,9 +110,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("Loaded {} battery bank definitions", banks.len());
 
     let max_jolt:u32 = banks.iter().map(|b| b.max_joltage()).sum();
-
-    println!("Maximum joltage is {}", max_jolt);
-
+    println!("Old maximum joltage is {}", max_jolt);
+    let max_jolt_v2:u64 = banks.iter().map(|b| b.max_joltage_v2()).sum();
+    println!("New maximum joltage is {}", max_jolt_v2);
     Ok( () )
 }
 
@@ -109,4 +143,32 @@ mod test {
         let bank = BatteryBank::from_string("818181911112111").unwrap();
         assert_eq!(bank.max_joltage(), 92);
     }
+
+    #[test]
+    fn test_example1v2() {
+        let bank = BatteryBank::from_string("987654321111111").unwrap();
+        assert_eq!(bank.max_joltage_v2(), 987654321111);
+    }
+
+    #[test]
+    fn test_example2v2() {
+        let bank = BatteryBank::from_string("811111111111119").unwrap();
+        assert_eq!(bank.max_joltage_v2(), 811111111119);
+    }
+
+
+    #[test]
+    fn test_example2v3() {
+        let bank = BatteryBank::from_string("234234234234278").unwrap();
+        assert_eq!(bank.max_joltage_v2(), 434234234278);
+    }
+
+
+    #[test]
+    fn test_example2v4() {
+        let bank = BatteryBank::from_string("818181911112111").unwrap();
+        assert_eq!(bank.max_joltage_v2(), 888911112111);
+    }
+
+
 }
