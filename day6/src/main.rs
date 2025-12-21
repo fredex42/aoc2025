311b1afe0be55eb6b0eeb24b91a215d1a5b8f8b9
Andy Gallagher <fredex42@gmail.com>
2025-12-21 09:15:15 +0000

    working on new parser for part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, fs::File, io::Read, num::ParseIntError};
+use std::{error::Error, fs::File, io::Read, num::ParseIntError, ptr::null};
 use regex::Regex;
 use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
@@ -27,6 +27,38 @@ impl MathProblem {
     }
 }
 
+/**
+ * Takes in a 2d array of type T, and swaps rows for columns; returning the result as a new 2d array
+ * Performs some basic sanity checks and returns an error if it fails
+ */
+pub fn transpose<T: Copy>(values:&Vec<Vec<T>>, null_value:T) -> Result<Vec<Vec<T>>, Box<dyn Error>> {
+    //We know the sizes, so pre-allocate
+    if values.is_empty() {
+        return Err("there was no data to parse".into());
+    }
+
+    let row_count = values.len();
+    let col_count = values[0].len();
+    //Sanity-check col_count
+    if values.iter().any(|row| {
+        row.len()!=col_count
+    }) {
+        return Err("the incoming data was not square: {}".into());
+    }
+    let mut new_shape:Vec<Vec<T>> = vec![vec![null_value; row_count]; col_count];
+
+    //Flip them over into the new vecs
+    for row_idx in 0..(&values).len() {
+        let row = &values[row_idx];
+        
+        for col_idx in 0..row.len() {
+            new_shape[col_idx][row_idx] = values[row_idx][col_idx];
+        }
+    }
+
+    Ok( new_shape )
+}
+
 pub fn parse_input(input:&str) -> Result<Vec<MathProblem>, Box<dyn Error>> {
     let is_space = Regex::new("\\s+").unwrap();
 
@@ -39,30 +71,8 @@ pub fn parse_input(input:&str) -> Result<Vec<MathProblem>, Box<dyn Error>> {
         .collect();
 
     //OK, so values currently goes row -> column (outer to inner).  We need to reverse it, into column -> row (outer to inner)
+    let new_shape = transpose(&values,"")?;
     
-    //We know the sizes, so pre-allocate
-    if values.is_empty() {
-        return Err("there was no data to parse".into());
-    }
-    let row_count = values.len();
-    let col_count = values[0].len();
-    //Sanity-check col_count
-    if values.iter().any(|row| {
-        row.len()!=col_count
-    }) {
-        return Err("the incoming data was not square: {}".into());
-    }
-    let mut new_shape:Vec<Vec<&str>> = vec![vec![""; row_count]; col_count];
-
-    //Flip them over into the new vecs
-    for row_idx in 0..(&values).len() {
-        let row = &values[row_idx];
-        
-        for col_idx in 0..row.len() {
-            new_shape[col_idx][row_idx] = values[row_idx][col_idx];
-        }
-    }
-
     //Now construct the domain objects
     let mut results:Vec<MathProblem> = Vec::with_capacity(new_shape.len());
     for col in new_shape {
@@ -87,6 +97,43 @@ pub fn parse_input(input:&str) -> Result<Vec<MathProblem>, Box<dyn Error>> {
         }
     }
     Ok( results )
+}
+
+pub fn parse_input_v2(input:&str) -> Result<Vec<MathProblem>, Box<dyn Error>> {
+    let lines:Vec<&str> = input.split("\n").filter(|l| l.len()>2).collect();
+    
+    //We can use the final line (operations) as the key, since each operation char lines up with
+    //the first column of the numbers
+    let col_key:Vec<usize> = match lines.last().map(|last_line| {
+            let anychar = Regex::new("([^\\s])").unwrap();
+            anychar.captures_iter(last_line).map(|c| c.get_match().start()).collect()
+    }) {
+        Some(key)=>key,
+        None=>return Err("there was not enough data to determine a key".into())
+    };
+
+    println!("col_key is {:?}", col_key);
+
+    //Now slice the lines to get the entries
+    let entries:Vec<Vec<&str>> = lines.iter().map(|line| {
+        let mut line_entries:Vec<&str> = Vec::with_capacity(col_key.len());
+        for i in 0..col_key.len() {
+            let from = col_key[i];
+            let to = if i==col_key.len()-1 {
+                line.len()
+            } else {
+                col_key[i+1]
+            };
+            
+            println!("from {} to {}", from, to);
+            line_entries.push(&line[from..to]);
+        }
+        line_entries
+    }).collect();
+
+    println!("Got entries: {:?}", entries);
+
+    Ok( vec![] )
 }
 
 fn main() -> Result<(), Box<dyn Error>>{
@@ -128,5 +175,15 @@ mod test {
         let probs = parse_input(&input).unwrap();
         let final_result:i64 = probs.par_iter().map(|p| p.calculate().expect("the problem was empty?")).sum();
         assert_eq!(final_result, 4277556);
+    }
+
+    #[test]
+    fn test_example_v2() {
+        let input = "123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  ";
+        let _ = parse_input_v2(&input).unwrap();
+
     }
 }
