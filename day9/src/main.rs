2af41461ed6aa048ef1ea8068b3e612af3a882e7
Andy Gallagher <fredex42@gmail.com>
2026-01-04 16:24:44 +0000

    simpler looking for basic crossing of edges

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -2,7 +2,7 @@ use std::{error::Error, fs::File, io::Read};
 use rayon::iter::{IntoParallelIterator, IntoParallelRefIterator, ParallelIterator};
 use regex::Regex;
 
-#[derive(Copy, Clone, PartialEq, Eq)]
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
 pub struct Tile {
     x: i64,
     y: i64
@@ -43,6 +43,20 @@ impl TilePair<'_> {
             (self.tile_a.x - self.tile_b.x + 1).abs() * (self.tile_a.y - self.tile_b.y + 1).abs()
         ).try_into().expect("there was an integer overflow calculating area")
     }
+
+    pub fn edges_of_rectangle(&self) -> Vec<Edge> {
+        let tl = Tile { x: self.tile_a.x.min(self.tile_b.x), y: self.tile_a.y.min(self.tile_b.y)};
+        let bl = Tile { x: self.tile_a.x.min(self.tile_b.x), y: self.tile_a.y.max(self.tile_b.y)};
+        let tr = Tile { x: self.tile_a.x.max(self.tile_b.x), y: self.tile_a.y.min(self.tile_b.y)};
+        let br = Tile { x: self.tile_a.x.max(self.tile_b.x), y: self.tile_b.y.max(self.tile_b.y)};
+
+        vec![
+            Edge { start: tl, end: tr},
+            Edge { start: bl, end: br},
+            Edge { start: tl, end: bl},
+            Edge { start: tr, end: br}
+        ]
+    }
 }
 
 impl PartialOrd for TilePair<'_> {
@@ -66,6 +80,102 @@ impl Ord for TilePair<'_> {
         } else {
             std::cmp::Ordering::Equal
         }
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub struct Edge {
+    start: Tile,
+    end: Tile
+}
+
+impl Edge {
+    pub fn new(start: &Tile, end: &Tile) -> Edge {
+        Edge { start: *start, end: *end }
+    }
+
+    /**
+     * Returns true if the other edge is at 90 degrees to this one.
+     * Since all edges in the problem are axis aligned we only check horizontal and vertical
+     */
+    pub fn is_perpendicular(&self, other: &Edge) -> bool {
+        (self.start.x==self.end.x && other.start.y==other.end.y) || (self.start.y==self.end.y && other.start.x==other.end.x)
+    }
+
+    pub fn min_x(&self) -> i64 {
+        return self.start.x.min(self.end.x)
+    }
+
+    pub fn max_x(&self) -> i64 {
+        return self.start.x.max(self.end.x)
+    }
+
+    pub fn min_y(&self) -> i64 {
+        self.start.y.min(self.end.y)
+    }
+
+    pub fn max_y(&self) -> i64 {
+        self.start.y.max(self.end.y)
+    }
+
+    pub fn is_vertical(&self) -> bool {
+        self.start.x==self.end.x
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Perimeter {
+    edges: Vec<Edge>
+}
+
+impl Perimeter {
+        /**
+     * Constructs a perimeter from the given control points
+     */
+    pub fn new<'a>(control_points: &'a Vec<Tile>) -> Option<Perimeter> {
+        if control_points.len() < 3 {
+            return None
+        }
+
+        let mut edges: Vec<Edge> = vec![];
+        
+        //Let's assume that the control_points vec is already correctly ordered
+        let (start_point, remaining_points) = control_points.split_first().unwrap();    //unwrap is safe as we already checked is_empty
+        let mut next_point = start_point;
+        for cp in remaining_points {
+            let next_edge = Edge::new(next_point, cp);
+            edges.push(next_edge);
+            next_point = cp;
+        }
+        let end = control_points.last().unwrap();
+        let final_edge = Edge::new(end, start_point);
+        edges.push(final_edge);
+
+        Some(Perimeter { edges })
+    }
+
+    /**
+     * Returns true if the given edge crosses any segment of the perimeter.
+     * Touching is fine.
+     */
+    pub fn edge_crosses(&self, edge: &Edge) -> bool {
+        self.edges.par_iter()
+            .filter(|perim_edge| perim_edge.is_perpendicular(edge))
+            .any(|perim_edge| {
+                if edge.is_vertical() {
+                    let x = edge.start.x;
+                    let y = perim_edge.start.y;
+
+                    x > perim_edge.min_x() && x < perim_edge.max_x() &&
+                    y > edge.min_y()      && y < edge.max_y()
+                } else {
+                    let y = edge.start.y;
+                    let x = perim_edge.start.x;
+
+                    y > perim_edge.min_y() && y < perim_edge.max_y() &&
+                    x > edge.min_x()       && x < edge.max_x()
+                }
+            })
     }
 }
 
@@ -104,10 +214,24 @@ fn main() ->Result<(), Box<dyn Error>> {
     let tiles = parse_input(&content)?;
     let mut pairs = pair_up(&tiles);
     pairs.sort();
+    pairs.reverse();
 
-    match pairs.last() {
+    match pairs.first() {
         Some(last_pair)=>println!("The largest area is {}", last_pair.area_of_rectangle()),
         None=>println!("ERROR! The list of pairs was empty :-/")
+    }
+
+    let perimeter = Perimeter::new(&tiles).expect("Could not construct perimeter");
+
+    let largest_fitting_rect = pairs.iter()
+        .filter(|rect| {
+            let crosses_perimeter = rect.edges_of_rectangle().par_iter().any(|edge| perimeter.edge_crosses(edge));
+            ! crosses_perimeter
+        }).next();
+    
+    match largest_fitting_rect {
+        Some(rect)=>println!("The largest rectangle that fitted was {}", rect.area_of_rectangle()),
+        None=>println!("No available rectangles fitted")
     }
     Ok( () )
 }
